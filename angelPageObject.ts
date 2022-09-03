f070faf65d3ca09c35ef3ad69d2bb9bcbbff2698
toangelc <toangelchen@gmail.com>
2022-09-03 00:46:35 -0600

    Angel's tests for logIn, signUp, forgotPassword with screenshots

--- angelPageObject.ts
+++ angelPageObject.ts
@@ -1,6 +1,7 @@
 import {BasePage} from './basePage'
 import {By} from 'selenium-webdriver'
 
+
 export class AngelPage extends BasePage {
     login: By = By.xpath('//*[@id="app"]/div/div[4]/div[2]/div/div/div[2]/header/div[1]/div[9]/div/div/div[1]/a/span')
     userName: By = By.name('username')
@@ -12,16 +13,10 @@ export class AngelPage extends BasePage {
     forgotPassword: By = By.xpath('//*[@id="app"]/div/div[2]/div/div/div/div[1]/div/form/div[7]/a')
     sendResetLink: By = By.xpath('//*[@id="app"]/div/div[2]/div/div/div/div[1]/div/div/div[2]/button')
     close: By = By.xpath('//*[@id="app"]/div/div[2]/div/div/div/div[2]/span')
-    searchBar: By = By.name('q')
-    results: By = By.id('rso')
+   
     
     constructor() {
         super({url: "https://drop.com/"})
-    }
-    async search(searchTerm:string) {
-        return this.setInput(this.searchBar, `${searchTerm}\n`)
-    }
-    async getResults() {
-        return this.getText(this.results)
+  
     }
 }
--- angelTest.test.ts
+++ angelTest.test.ts
@@ -2,13 +2,19 @@ import {AngelPage} from './angelPageObject'
 import { testEnvironment } from './jest.config'
 const angel = new AngelPage()
 
+   
 test('log in', async () => {
     await angel.navigate()
     await angel.click(angel.login)
     await angel.setInput(angel.userName, 'toangelc@yahoo.com.tw')
     await angel.setInput(angel.password, '1029')
+    await angel.driver.sleep(1000);
+    angel.driver.takeScreenshot().then(
+        function (image) {
+            require('fs').writeFileSync('logIn.screenshot.png', image, 'base64')
+        });
     await angel.click(angel.loginBtn)
-    
+        
 })
 
 test('sign up', async () => {
@@ -16,9 +22,13 @@ test('sign up', async () => {
     await angel.click(angel.signUp)
     await angel.setInput(angel.Email, 'angel@needake.com')
     await angel.setInput(angel.password, '1029')
+    await angel.driver.sleep(1000);
+    angel.driver.takeScreenshot().then(
+        function (image) {
+            require('fs').writeFileSync('signUp.screenshot.png', image, 'base64')
+        });
     await angel.click(angel.continue)
-
-})
+    })
 
 test('forgot password', async () => {
     await angel.navigate()
@@ -27,9 +37,12 @@ test('forgot password', async () => {
     await angel.setInput(angel.password, '5555')
     await angel.click(angel.loginBtn) 
     await angel.click(angel.forgotPassword)
-    
     await angel.click(angel.sendResetLink)
+    await angel.driver.sleep(1000);
+    angel.driver.takeScreenshot().then(
+        function (image) {
+            require('fs').writeFileSync('resetLinkSent.screenshot.png', image, 'base64')
+        });
     await angel.click(angel.close)
-    await angel.driver.sleep(40000)
     await angel.driver.quit()
 })
